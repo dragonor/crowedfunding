dce8a7ffd2206b1c81a759ab51efd8a8883cb657
zwlfromcn <zwlfromcn@gmail.com>
2021-04-13 17:17:44 +0800

    1 在spring中配置SqlSessionFactoryBean（dao接口的动态代理工厂）</br> 2 在spring中配置MapperScannerConfigurer（指定Mapper配置文件）</br> 3 测试是否成功把mapper接口代理对象载入IOC容器中，并且通过Mapper接口代理对象实现插入操作

--- crowdfunding_admin_parent/crowdfunding_admin_component/src/main/java/com/dragon/crowd/mapper/AdminMapper.java
+++ crowdfunding_admin_parent/crowdfunding_admin_component/src/main/java/com/dragon/crowd/mapper/AdminMapper.java
@@ -6,6 +6,7 @@ import com.dragon.crowd.entity.AdminExample;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 
 public interface AdminMapper {
     int countByExample(AdminExample example);
--- crowdfunding_admin_parent/crowdfunding_admin_webui/src/test/java/com/dragon/crowdfunding/test/CrowdTest.java
+++ crowdfunding_admin_parent/crowdfunding_admin_webui/src/test/java/com/dragon/crowdfunding/test/CrowdTest.java
@@ -1,5 +1,7 @@
 package com.dragon.crowdfunding.test;
 
+import com.dragon.crowd.entity.Admin;
+import com.dragon.crowd.mapper.AdminMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +12,7 @@ import javax.sql.DataSource;
 import java.sql.SQLException;
 
 /**
- * @Description TODO
+ * @Description 在webui中，测试数据库的连接，测试模块之间的关联性
  * @Author Dragon
  * @Date 2021/4/13
  * @Version 1.0.0
@@ -26,6 +28,19 @@ public class CrowdTest {
     @Test
     public void test() throws SQLException {
         System.out.println(dataSource.getConnection());
+    }
+
+    @Autowired
+    private AdminMapper adminMapper;
+//    private AdminService adminService;
+
+    @Test
+    public void testAdminMapperAutowired() {
+//        Admin admin = adminService.getAdminById(1);
+//        System.out.println(admin);
+        Admin admin = new Admin(null, "zwl", "123", "dragon", "zwl@163.com", null);
+        int count = adminMapper.insert(admin);
+        System.out.println("影响条数"+ count);
     }
 
 }
